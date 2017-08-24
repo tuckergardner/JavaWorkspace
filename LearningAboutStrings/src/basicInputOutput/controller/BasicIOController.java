04f6d82f1a073ab64d2fbeeab392891f91e745ba
ceb-student <CEB-Student@caddoschools.org>
2017-08-24 09:18:17 -0500

    basic
    basic

---
+++ LearningAboutStrings/src/basicInputOutput/controller/BasicIOController.java
@@ -0,0 +1,13 @@
+package basicInputOutput.controller;
+
+public class BasicIOController 
+{
+	public void printGreeting()
+	{
+		String firstName = "Tucker ";
+		String lastName = "Gardner";
+		System.out.println(firstName + lastName);
+	}
+	
+	
+}
---
+++ LearningAboutStrings/src/basicInputOutput/controller/BasicIORunner.java
@@ -0,0 +1,14 @@
+package basicInputOutput.controller;
+
+public class BasicIORunner
+{
+
+	public static void main(String[] args) 
+	{
+		BasicIOController myGreeting = new BasicIOController();
+		myGreeting.printGreeting();
+	
+
+	}
+
+}
