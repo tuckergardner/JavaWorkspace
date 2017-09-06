5598ff8621e75c249bc595aab323c4f7a853d1fe
ceb-student <CEB-Student@caddoschools.org>
2017-09-06 09:18:17 -0500

    alot of stuff...

---
+++ PlayingWithStrings/src/playingwithstrings/controller/Runner.java
@@ -0,0 +1,18 @@
+package playingwithstrings.controller;
+
+public class Runner 
+{
+
+	public static void main(String[] args)
+	{
+	
+
+		
+		
+		
+		
+		StringCommands myProgram = new StringCommands();
+		myProgram.charles();
+	}
+
+}
---
+++ PlayingWithStrings/src/playingwithstrings/controller/StringCommands.java
@@ -0,0 +1,26 @@
+package playingwithstrings.controller;
+import java.util.Scanner;
+
+public class StringCommands
+{
+	Scanner myScanner = new Scanner(System.in);
+	
+	public StringCommands()
+	{
+		
+	}
+	
+	public void charles()
+	{
+		System.out.println("Enter your name: ");
+		String reply = myScanner.nextLine();
+		System.out.println(reply);
+		if(reply.equals("charles"));		
+	}
+	system.out.println("hi charles");
+}
+}
+else
+{
+	system.out.print("idk");
+}
